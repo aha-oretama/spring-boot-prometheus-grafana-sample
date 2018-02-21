04e19c83fc750e04e14a87046f377119cf3aee14
yasufumi sekine <yasufumi_sekine@r.recruit.co.jp>
2018-02-21 12:11:46 +0900

    first commit

---
+++ src/main/java/jp/ahaoretama/springbootprometheusgrafanasample/SpringBootPrometheusGrafanaSampleApplication.java
@@ -0,0 +1,12 @@
+package jp.ahaoretama.springbootprometheusgrafanasample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPrometheusGrafanaSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPrometheusGrafanaSampleApplication.class, args);
+	}
+}
---
+++ src/test/java/jp/ahaoretama/springbootprometheusgrafanasample/SpringBootPrometheusGrafanaSampleApplicationTests.java
@@ -0,0 +1,16 @@
+package jp.ahaoretama.springbootprometheusgrafanasample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootPrometheusGrafanaSampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
